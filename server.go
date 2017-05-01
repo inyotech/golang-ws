3e9c612e8cefd37b8cb54209fb13325bcf058678
inyotech <scottb@inyotech.com>
2017-05-01 18:09:46 +0000

    Ubdate browser demo.

--- server.go
+++ server.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"html/template"
-	"time"
 	"github.com/inyotech/golang-ws/ws"
 
 )
@@ -18,45 +16,21 @@ func httpHandler(response http.ResponseWriter, request *http.Request) {
 	t.Execute(response, nil)
 }
 
-func handlews1(ch chan *ws.Frame) {
+func handlews(ch chan *ws.Frame) {
 
 	for {
 		frame, ws_ok := <-ch
 		if !ws_ok {
-			fmt.Println("handlews1 channel closed")
 			return
 		}
-		fmt.Println(string(frame.Payload))
-		frame.Payload = append([]byte("handlews1 "), frame.Payload...)
+		frame.Payload = append([]byte("received: "), frame.Payload...)
 		ch<-frame
 	}
 }
 
-func handlews2(ch chan *ws.Frame) {
-
-	for i:=0;i<3;i++ {
-		select {
-		case frame, ws_ok := <-ch:
-			if !ws_ok {
-				fmt.Println("handlews2 channel closed")
-				return
-			}
-			fmt.Println(string(frame.Payload))
-		case <-time.After(time.Second):
-			frame := ws.NewTextFrame("message from handlerws2")
-			fmt.Println(string(frame.Payload))
-			ch<-frame
-		}
-	}
-	fmt.Println("client closing channel")
-	close(ch)
-}
-
-
 func main() {
 
-	http.Handle("/service1", ws.WsHandlerFunc(handlews1))
-	http.Handle("/service2", ws.WsHandlerFunc(handlews2))
+	http.Handle("/service", ws.WsHandlerFunc(handlews))
 	http.HandleFunc("/", httpHandler)
 	http.ListenAndServe(":8080", nil)
 
