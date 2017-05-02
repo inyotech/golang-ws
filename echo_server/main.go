b1075e1dc384d42ab28b31abacd5dd42d7ae4e62
inyotech <scottb@inyotech.com>
2017-05-02 16:49:40 +0000

    Add command line flags. Use template interpolation to make ws url configurable.

--- echo_server/main.go
+++ echo_server/main.go
@@ -1,22 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"flag"
 	"net/http"
 	"html/template"
 
 	"github.com/inyotech/golang-ws/ws"
 )
 
-func httpHandler(response http.ResponseWriter, request *http.Request) {
+func (options options) httpHandler(response http.ResponseWriter, request *http.Request) {
+
 	t, err := template.ParseFiles("template/index.html")
 	if err != nil {
 		panic(err)
 	}
 
-	t.Execute(response, nil)
+	t.Execute(response, options)
 }
 
-func handlews(ch chan *ws.Frame) {
+func (options options) wsHandler(ch chan *ws.Frame) {
+
+	fmt.Println(options)
 
 	for {
 		frame, ws_ok := <-ch
@@ -27,10 +33,32 @@ func handlews(ch chan *ws.Frame) {
 	}
 }
 
+
+type options struct {
+	WorkingDir string
+	BindAddr string
+	WsUrl string
+}
+
 func main() {
 
-	http.Handle("/service", ws.WsHandlerFunc(handlews))
-	http.HandleFunc("/", httpHandler)
-	http.ListenAndServe(":8080", nil)
+	var options options
+
+	flag.StringVar(&options.WorkingDir, "WorkingDir", ".", "Working directory, file search path is relative to this (default '.')")
+	flag.StringVar(&options.BindAddr, "BindAddr", "localhost:9000", "Ip:Port to listen on (default 'localhost:9000')")
+	flag.StringVar(&options.WsUrl, "WsUrl", "ws://localhost:9000/", "Web socket base url (default 'ws://localhost:9000/')")
+
+	flag.Parse()
+
+	fmt.Println(options)
+
+	err := os.Chdir(options.WorkingDir)
+	if err != nil {
+		panic(err)
+	}
+
+	http.Handle("/echo_service", ws.WsHandlerFunc(options.wsHandler))
+	http.HandleFunc("/", options.httpHandler)
+	http.ListenAndServe(options.BindAddr, nil)
 
 }
